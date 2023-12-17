93417548cddec0ee42ffebd733b306dc234aa8c9
Vy Pham <thuvytpham@gmail.com>
2023-12-17 15:26:08 -0500

    better bg and bubbles wrapped

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,24 +10,29 @@ function App() {
 
   return (
     <>
-      <div className="h-32">
-        <h1 className="text-blue-950">To-Do</h1>
+      <div className="top">
+        <div className="flex h-32 justify-center">
+          <h1 className="text-blue-950 bg-white rounded-3xl w-52 h-fit">
+            To-Do
+          </h1>
+        </div>
+
+        <div>
+          <button
+            className=" text-white m-14"
+            onClick={() => setTasks([...tasks, "edit task"])}
+          >
+            New
+          </button>
+        </div>
       </div>
 
-      <div>
-        <button
-          className=" text-white m-14"
-          onClick={() => setTasks([...tasks, "edit task"])}
-        >
-          New
-        </button>
-      </div>
       {console.log(tasks)}
-      <div className="flex items-center justify-center">
+      <body className="flex items-center justify-center flex-wrap">
         {tasks.map((taskText, i) => (
           <Task task={taskText} />
         ))}
-      </div>
+      </body>
     </>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,7 +5,7 @@ export default function Task(props) {
   const [task, setTask] = useState("add task");
 
   return (
-    <div className="flex items-center justify-center bg-white w-44 h-44 rounded-full hover:bg-blue-50 m-5">
+    <div className="flex items-center justify-center bg-white w-44 h-44 rounded-full hover:bg-slate-900 hover:text-white hover:outline outline-white 0 m-5">
       <p className="break-all w-40">{props.task}</p>
     </div>
   );
